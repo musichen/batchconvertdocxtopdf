88a2343d0d33ff29994c65dd3141e5521b164bed
Alexander Musichen <alex.musichen@gmail.com>
2023-03-21 12:51:49 +0000

    initialize project batch convert docx to pdf   CLI tool

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # batchconvertdocxtopdf-
 batch convert docx to pdf   CLI tool
+
+# make sure PANDOC is installed! visit https://pandoc.org/installing.html to install 
+
+# make sure pdflatex is installed 
+
+# navigate to folder where you want to convert docx to pdf in batch, run
+
+# python batchconvertdocx2pdf.py 'path_to_folder_with_docx_INPUT' 'path_to_folder_where_to_put_PDF'
---
+++ batchconvertdocx2pdf.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+import shutil
+import subprocess
+
+# make sure PANDOC is installed! visit https://pandoc.org/installing.html to install 
+# make sure pdflatex is installed   just visit https://www.tug.org/begin.html   or if your're using Mac then install it with brew
+# brew install mactex 
+
+
+# Check if Pandoc is installed
+try:
+    subprocess.run(['pandoc', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except FileNotFoundError:
+    print('Pandoc is not installed. Please visit https://pandoc.org/installing.html to install.')
+    exit()
+
+# Check if pdflatex is installed
+try:
+    subprocess.run(['pdflatex', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except FileNotFoundError:
+    print('pdflatex is not installed. Please install a LaTeX distribution, such as TeX Live (https://www.tug.org/texlive/) or MiKTeX (https://miktex.org/).')
+    exit()
+
+
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Batch convert docx files to pdf')
+parser.add_argument('input_folder', type=str, help='path to the input folder containing docx files')
+parser.add_argument('output_folder', type=str, help='path to the output folder for pdf files')
+args = parser.parse_args()
+
+# Loop over the docx files in the input folder
+for filename in os.listdir(args.input_folder):
+    if filename.endswith('.docx'):
+        # Generate the output filename by replacing the extension
+        output_filename = os.path.splitext(filename)[0] + '.pdf'
+        
+        # Call Pandoc to convert the input file to PDF
+        input_path = os.path.join(args.input_folder, filename)
+        output_path = os.path.join(args.output_folder, output_filename)
+        os.system(f'pandoc "{input_path}" -o "{output_path}"')
+
+
+import os
+import argparse
+
+
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Batch convert docx files to pdf')
+parser.add_argument('input_folder', type=str, help='path to the input folder containing docx files')
+parser.add_argument('output_folder', type=str, help='path to the output folder for pdf files')
+args = parser.parse_args()
+
+# Loop over the docx files in the input folder
+for filename in os.listdir(args.input_folder):
+    if filename.endswith('.docx'):
+        # Generate the output filename by replacing the extension
+        output_filename = os.path.splitext(filename)[0] + '.pdf'
+
+        # Call Pandoc to convert the input file to PDF
+        input_path = os.path.join(args.input_folder, filename)
+        output_path = os.path.join(args.output_folder, output_filename)
+        subprocess.run(['pandoc', input_path, '-o', output_path])
